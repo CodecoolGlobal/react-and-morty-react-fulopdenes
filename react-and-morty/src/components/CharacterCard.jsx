fa6e14674560ffb54357d242e159c3768367ef70
Dénes <denes.fulopp@gmail.com>
2022-10-10 15:47:58 +0200

    show data

--- react-and-morty/src/components/CharacterCard.jsx
+++ react-and-morty/src/components/CharacterCard.jsx
@@ -1,12 +1,16 @@
 
 export default function CharacterCard (props) {
     if (props.character.results !== undefined) {
-         console.log(props.character.results[0]);
-    }
-    
-    return (
+         return (
         <>
-        <p>TEST</p>
+        <div className="character-Card">
+            <div>{props.character.results[0].id}</div>
+            <div>{props.character.results[0].name}</div>
+            <div>{props.character.results[0].status}</div>
+        </div>
         </>
     )
+    }
+    
+    
 }

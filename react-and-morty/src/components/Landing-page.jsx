2fb7664f5979985ec33c89febc02274461829803
Farouq Siyar <siyar9989@gmail.com>
2022-10-10 16:46:58 +0200

    size up landing image

--- react-and-morty/src/components/Landing-page.jsx
+++ react-and-morty/src/components/Landing-page.jsx
@@ -6,7 +6,7 @@ export default function LandingPage () {
     return (
         <>
         <div>
-            <img src="https://images2.alphacoders.com/642/642540.png" alt="logo" width={500}></img>
+            <img src="https://images2.alphacoders.com/642/642540.png" alt={"logo"} id="landing-image"></img>
         <Link to="/characters">
           <button>CHARACTHERS</button>
         </Link>

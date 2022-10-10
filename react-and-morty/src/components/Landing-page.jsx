41882b63bdf7bdd69ebed01d7b7b5d4df57af63c
miz092 <mihalyfi.zoltan@gmail.com>
2022-10-10 18:10:21 +0200

    add pagination component and puts page number into that component

--- react-and-morty/src/components/Landing-page.jsx
+++ react-and-morty/src/components/Landing-page.jsx
@@ -1,20 +1,24 @@
-
 import { Outlet, Link } from "react-router-dom";
 
-export default function LandingPage () {
-
-    return (
-        <>
-        <div>
-            <img src="https://images2.alphacoders.com/642/642540.png" alt={"logo"} id="landing-image"></img>
+export default function LandingPage() {
+  return (
+    <>
+      <div>
+        <img
+          src="https://images2.alphacoders.com/642/642540.png"
+          alt={"logo"}
+          id="landing-image"
+        ></img>
         <Link to="/characters">
           <button>CHARACTHERS</button>
         </Link>
         <Link to="/locations">
           <button>LOCATIONS</button>
         </Link>
+        <div> Lorem ipsum dolor sit amet </div>
       </div>
-     <Outlet />
-        </>
-    )
+
+      <Outlet />
+    </>
+  );
 }

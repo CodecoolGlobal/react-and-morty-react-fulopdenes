3ead61a85930852b5ce81828ad94725e85c3e1ce
Farouq Siyar <siyar9989@gmail.com>
2022-10-11 11:00:29 +0200

    main card info disappear on click

--- react-and-morty/src/components/CharacterCards.jsx
+++ react-and-morty/src/components/CharacterCards.jsx
@@ -1,37 +1,42 @@
 import { useState } from "react";
 import "./CharacterCards.css";
 
-const Card = props => {
+const Card = (props) => {
   const [isClicked, setIsClicked] = useState(false);
   const char = props.char;
   return (
-    <div onClick={() => setIsClicked(!isClicked)} className={isClicked ? "character-Card clicked": "character-Card"} key={char.id}>
+    <div
+      onClick={() => setIsClicked(!isClicked)}
+      className={isClicked ? "character-Card clicked" : "character-Card"}
+      key={char.id}
+    >
       <div className="char-name">{char.name}</div>
       <img
         src={char.image}
         className={
-            char.status === "Dead" ?
-            "char-image status-dead" :
-            char.status === "unknown" ? 
-            "char-image status-unknown" :
-            "char-image status-alive"
-          }
+          char.status === "Dead"
+            ? "char-image status-dead"
+            : char.status === "unknown"
+            ? "char-image status-unknown"
+            : "char-image status-alive"
+        }
         alt={char.name}
       ></img>
       <div
         className={
-          char.status === "Dead" ?
-          "status-dead" :
-          char.status === "unknown" ? 
-          "status-unknown" :
-          "status-alive"
+          char.status === "Dead"
+            ? "status-dead"
+            : char.status === "unknown"
+            ? "status-unknown"
+            : "status-alive"
         }
       >
         {char.status}
       </div>
+      <div className="more-info-div">Dingdong</div>
     </div>
-  )
-}
+  );
+};
 
 export default function CharacterCards(props) {
   const character = props.character.results;
@@ -39,7 +44,9 @@ export default function CharacterCards(props) {
   if (character !== undefined) {
     return (
       <>
-        {character.map((char) => <Card char={char}/>)}
+        {character.map((char) => (
+          <Card char={char} />
+        ))}
       </>
     );
   }

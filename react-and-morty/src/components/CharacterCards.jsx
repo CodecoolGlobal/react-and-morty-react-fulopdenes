dcb269236540527e6140f0280781313fe5c465a4
BrownieMaar <barnamarci07@gmail.com>
2022-10-11 10:41:11 +0200

    test adding ternary classes ti components

--- react-and-morty/src/components/CharacterCards.jsx
+++ react-and-morty/src/components/CharacterCards.jsx
@@ -1,4 +1,37 @@
+import { useState } from "react";
 import "./CharacterCards.css";
+
+const Card = props => {
+  const [isClicked, setIsClicked] = useState(false);
+  const char = props.char;
+  return (
+    <div onClick={() => setIsClicked(!isClicked)} className={isClicked ? "character-Card clicked": "character-Card"} key={char.id}>
+      <div className="char-name">{char.name}</div>
+      <img
+        src={char.image}
+        className={
+            char.status === "Dead" ?
+            "char-image status-dead" :
+            char.status === "unknown" ? 
+            "char-image status-unknown" :
+            "char-image status-alive"
+          }
+        alt={char.name}
+      ></img>
+      <div
+        className={
+          char.status === "Dead" ?
+          "status-dead" :
+          char.status === "unknown" ? 
+          "status-unknown" :
+          "status-alive"
+        }
+      >
+        {char.status}
+      </div>
+    </div>
+  )
+}
 
 export default function CharacterCards(props) {
   const character = props.character.results;
@@ -6,35 +39,7 @@ export default function CharacterCards(props) {
   if (character !== undefined) {
     return (
       <>
-        {character.map((char) => {
-          return (
-            <div className="character-Card" key={char.id}>
-              <div className="char-name">{char.name}</div>
-              <img
-                src={char.image}
-                className={
-                    char.status === "Dead" ?
-                    "char-image status-dead" :
-                    char.status === "unknown" ? 
-                    "char-image status-unknown" :
-                    "char-image status-alive"
-                  }
-                alt={char.name}
-              ></img>
-              <div
-                className={
-                  char.status === "Dead" ?
-                  "status-dead" :
-                  char.status === "unknown" ? 
-                  "status-unknown" :
-                  "status-alive"
-                }
-              >
-                {char.status}
-              </div>
-            </div>
-          );
-        })}
+        {character.map((char) => <Card char={char}/>)}
       </>
     );
   }

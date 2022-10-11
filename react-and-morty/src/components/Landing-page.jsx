bc2dd86d3d40c4730bc21ab054192e87f32e5333
Farouq Siyar <siyar9989@gmail.com>
2022-10-11 16:29:22 +0200

    redesign landing page

--- react-and-morty/src/components/Landing-page.jsx
+++ react-and-morty/src/components/Landing-page.jsx
@@ -5,13 +5,14 @@ export default function LandingPage() {
     <>
       <div className="landing-page">
             <div id="title-text">Morty look! A well designed welcome page!</div>
+            <div id="char-text">CHARACTERS</div>
           <Link to="/characters">
             <img src="https://media.tenor.com/BgR83Df82t0AAAAi/portal-rick-and-morty.gif" id="char-portal"></img>
           </Link>
           <Link to="/locations">
           <img src="https://media.tenor.com/BgR83Df82t0AAAAi/portal-rick-and-morty.gif" id="loc-portal"></img>
           </Link>
-        <div> Lorem ipsum dolor sit amet </div>
+          <div> Lorem ipsum dolor sit amet </div>
       </div>
 
       <Outlet />

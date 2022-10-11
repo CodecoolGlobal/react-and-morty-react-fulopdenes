b60c9e1327f979b55e1ad28702a20cf28feb62ad
BrownieMaar <barnamarci7@gmail.com>
2022-10-11 11:17:12 +0200

    refactor character card  into two seperate flexboxes

--- react-and-morty/src/components/CharacterCards.jsx
+++ react-and-morty/src/components/CharacterCards.jsx
@@ -7,33 +7,37 @@ const Card = (props) => {
   return (
     <div
       onClick={() => setIsClicked(!isClicked)}
-      className={isClicked ? "character-Card clicked" : "character-Card"}
+      className={isClicked ? "card-viewport clicked" : "card-viewport"}
       key={char.id}
     >
-      <div className="char-name">{char.name}</div>
-      <img
-        src={char.image}
-        className={
-          char.status === "Dead"
-            ? "char-image status-dead"
-            : char.status === "unknown"
-            ? "char-image status-unknown"
-            : "char-image status-alive"
-        }
-        alt={char.name}
-      ></img>
-      <div
-        className={
-          char.status === "Dead"
-            ? "status-dead"
-            : char.status === "unknown"
-            ? "status-unknown"
-            : "status-alive"
-        }
-      >
-        {char.status}
+      <div className="character-card">
+        <div className="main-info">
+          <div className="char-name">{char.name}</div>
+          <img
+            src={char.image}
+            className={
+              char.status === "Dead"
+                ? "char-image status-dead"
+                : char.status === "unknown"
+                  ? "char-image status-unknown"
+                  : "char-image status-alive"
+            }
+            alt={char.name}
+          ></img>
+          <div
+            className={
+              char.status === "Dead"
+                ? "status-dead"
+                : char.status === "unknown"
+                  ? "status-unknown"
+                  : "status-alive"
+            }
+          >
+            {char.status}
+          </div>
+          <div className="more-info-div">Dingdong</div>
+        </div>
       </div>
-      <div className="more-info-div">Dingdong</div>
     </div>
   );
 };

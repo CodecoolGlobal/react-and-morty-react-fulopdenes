395a4a5bae983144c60a4de0a400ce1aca284ecf
Farouq Siyar <siyar9989@gmail.com>
2022-10-12 00:04:53 +0200

    redesign landing page

--- react-and-morty/src/components/Landing-page.jsx
+++ react-and-morty/src/components/Landing-page.jsx
@@ -22,12 +22,11 @@ export default function LandingPage() {
           <div id="loc-text">LOCATIONS</div>
         </Link>
         <header className="landing-page-welcome-text-container">
-          <h4 className="drop-shadow">WELCOME AT</h4>
+          <h4 className="drop-shadow">WELCOME TO THE</h4>
           <h1 className="drop-shadow">RICK AND MORTY</h1>
           <h2 className="drop-shadow">WEBCATALOG</h2>
           <h5>
-            YOU CAN FIND MORE DETAILS ABOUT LOCATIONS AND CHARACTERS. PLEASE
-            SELECT A PORTAL …
+            PLEASE ENTER A PORTAL TO FIND MORE INFO ABOUT...
           </h5>
         </header>
       </div>

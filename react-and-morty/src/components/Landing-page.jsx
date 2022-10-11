c86319704ef4889d1b197175493232f4be9230b5
Dénes <denes.fulopp@gmail.com>
2022-10-11 16:30:32 +0200

    text

--- react-and-morty/src/components/Landing-page.jsx
+++ react-and-morty/src/components/Landing-page.jsx
@@ -1,22 +1,27 @@
 import { Outlet, Link } from "react-router-dom";
+import "./Landing-page.css";
 
 export default function LandingPage() {
   return (
     <>
       <div className="landing-page">
-            <div id="title-text">Morty look! A well designed welcome page!</div>
+        <div id="title-text">Morty look! A well designed welcome page!</div>
         <img
           src="https://images2.alphacoders.com/642/642540.png"
           alt={"logo"}
           id="landing-image"
         ></img>
-          <Link to="/characters">
+        <Link to="/characters">
           <button id="char-button">CHARACTERS</button>
-          </Link>
-          <Link to="/locations">
+        </Link>
+        <Link to="/locations">
           <button id="loc-button">LOCATIONS</button>
-          </Link>
-        <div> Lorem ipsum dolor sit amet </div>
+        </Link>
+        <header className="landing-page-welcome-text-container">
+          <h4>WELCOME AT</h4>
+          <h1>RICK AND MORTY</h1>
+          <h2>WEBCATALOG</h2>
+        </header>
       </div>
 
       <Outlet />

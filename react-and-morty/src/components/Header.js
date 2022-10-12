22a1568c1ba322b571144492e14fadd575e9254e
BrownieMaar <barnamarci7@gmail.com>
2022-10-12 10:29:13 +0200

    replace header logo image

--- react-and-morty/src/components/Header.js
+++ react-and-morty/src/components/Header.js
@@ -3,13 +3,14 @@ import "./Header.css";
 import headerImage from "../images/Header.png";
 import sunImage from "../images/sun gif.gif";
 import sunDown from "../images/down sun.gif";
+import rickAndMortyLogo from "../images/Rick-and-Morty.png";
 export default function Header() {
   return (
     <div className="header">
       <img
         alt="rick and morty logo"
         className="logo-image"
-        src="https://1000logos.net/wp-content/uploads/2022/03/Rick-and-Morty.png"
+        src={rickAndMortyLogo}
       ></img>
       <a href="#">
         <img

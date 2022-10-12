ac070a6d44b6e8ad7e5562ee6d7b95bca89edd5e
miz092 <mihalyfi.zoltan@gmail.com>
2022-10-12 09:23:24 +0200

    replace screaming sun to a gif

--- react-and-morty/src/components/Header.jsx
+++ react-and-morty/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Header.css";
 import headerImage from "../images/Header.png";
-import sunImage from "../images/Sun.png";
+import sunImage from "../images/sun gif.gif";
+import sunDown from "../images/down sun.gif";
 export default function Header() {
   return (
     <div className="header">
@@ -10,12 +11,20 @@ export default function Header() {
         className="logo-image"
         src="https://1000logos.net/wp-content/uploads/2022/03/Rick-and-Morty.png"
       ></img>
-      <img
-        className="sun-image"
-        id="sun-image"
-        src={sunImage}
-        alt={"screaming sun"}
-      ></img>
+      <a href="#">
+        <img
+          className="sunDown-image"
+          src={sunDown}
+          alt={"screaming sun"}
+          border={0}
+        ></img>
+        <img
+          className="sun-image"
+          src={sunImage}
+          alt={"screaming sun"}
+          border={0}
+        ></img>
+      </a>
       <img
         className="header-image"
         src={headerImage}

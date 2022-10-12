147ac2568905ab96b2636b18257762ae1bfa4822
Dénes <denes.fulopp@gmail.com>
2022-10-12 20:51:30 +0200

    add the tests  cases to the project

--- react-and-morty/src/App.test.js
+++ react-and-morty/src/App.test.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
-import Characters from "./components/Characters-page";
 import CharacterCards from "./components/CharacterCards";
+import LocationCards from "./components/LocationCards";
 
-// test("Unit test 1: the page loads successfully", () => {
-//   const inspectedData = render(<App />);
-//   expect(screen.getByText("RICK AND MORTY")).toBeInTheDocument();
-//   expect(inspectedData).toMatchSnapshot();
-// });
+test("THE PAGE LOADS SUCCESSFULLY", async () => {
+  const inspectedData = render(<App />);
+  expect(await screen.findByText("Webcatalog")).toBeInTheDocument();
+  expect(inspectedData).toMatchSnapshot();
+});
 
-test("Unit test 2: more information shows up from the character, when we click on a character in the list", () => {
+test("BY CLICKING AT CHARACTERCARD IN LIST, MORE INFO SHOW UP", () => {
   render(
     <CharacterCards
       character={{
@@ -32,59 +31,7 @@ test("Unit test 2: more information shows up from the character, when we click o
               url: "https://rickandmortyapi.com/api/location/3",
             },
             image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-            episode: [
-              "https://rickandmortyapi.com/api/episode/1",
-              "https://rickandmortyapi.com/api/episode/2",
-              "https://rickandmortyapi.com/api/episode/3",
-              "https://rickandmortyapi.com/api/episode/4",
-              "https://rickandmortyapi.com/api/episode/5",
-              "https://rickandmortyapi.com/api/episode/6",
-              "https://rickandmortyapi.com/api/episode/7",
-              "https://rickandmortyapi.com/api/episode/8",
-              "https://rickandmortyapi.com/api/episode/9",
-              "https://rickandmortyapi.com/api/episode/10",
-              "https://rickandmortyapi.com/api/episode/11",
-              "https://rickandmortyapi.com/api/episode/12",
-              "https://rickandmortyapi.com/api/episode/13",
-              "https://rickandmortyapi.com/api/episode/14",
-              "https://rickandmortyapi.com/api/episode/15",
-              "https://rickandmortyapi.com/api/episode/16",
-              "https://rickandmortyapi.com/api/episode/17",
-              "https://rickandmortyapi.com/api/episode/18",
-              "https://rickandmortyapi.com/api/episode/19",
-              "https://rickandmortyapi.com/api/episode/20",
-              "https://rickandmortyapi.com/api/episode/21",
-              "https://rickandmortyapi.com/api/episode/22",
-              "https://rickandmortyapi.com/api/episode/23",
-              "https://rickandmortyapi.com/api/episode/24",
-              "https://rickandmortyapi.com/api/episode/25",
-              "https://rickandmortyapi.com/api/episode/26",
-              "https://rickandmortyapi.com/api/episode/27",
-              "https://rickandmortyapi.com/api/episode/28",
-              "https://rickandmortyapi.com/api/episode/29",
-              "https://rickandmortyapi.com/api/episode/30",
-              "https://rickandmortyapi.com/api/episode/31",
-              "https://rickandmortyapi.com/api/episode/32",
-              "https://rickandmortyapi.com/api/episode/33",
-              "https://rickandmortyapi.com/api/episode/34",
-              "https://rickandmortyapi.com/api/episode/35",
-              "https://rickandmortyapi.com/api/episode/36",
-              "https://rickandmortyapi.com/api/episode/37",
-              "https://rickandmortyapi.com/api/episode/38",
-              "https://rickandmortyapi.com/api/episode/39",
-              "https://rickandmortyapi.com/api/episode/40",
-              "https://rickandmortyapi.com/api/episode/41",
-              "https://rickandmortyapi.com/api/episode/42",
-              "https://rickandmortyapi.com/api/episode/43",
-              "https://rickandmortyapi.com/api/episode/44",
-              "https://rickandmortyapi.com/api/episode/45",
-              "https://rickandmortyapi.com/api/episode/46",
-              "https://rickandmortyapi.com/api/episode/47",
-              "https://rickandmortyapi.com/api/episode/48",
-              "https://rickandmortyapi.com/api/episode/49",
-              "https://rickandmortyapi.com/api/episode/50",
-              "https://rickandmortyapi.com/api/episode/51",
-            ],
+            episode: ["https://rickandmortyapi.com/api/episode/1"],
             url: "https://rickandmortyapi.com/api/character/1",
             created: "2017-11-04T18:48:46.250Z",
           },
@@ -92,5 +39,36 @@ test("Unit test 2: more information shows up from the character, when we click o
       }}
     />
   );
-  screen.debug();
+
+  fireEvent.click(screen.getByRole("show-more-data"));
+
+  expect(screen.getByRole("show-more-data").className).toBe(
+    "char-card-viewport clicked"
+  );
+});
+
+test("BY CLICKING AT LOCATIONCARD IN LIST, MORE INFO SHOW UP", () => {
+  render(
+    <LocationCards
+      location={{
+        results: [
+          {
+            id: 1,
+            name: "Earth (C-137)",
+            type: "Planet",
+            dimension: "Dimension C-137",
+            residents: ["https://rickandmortyapi.com/api/character/38"],
+            url: "https://rickandmortyapi.com/api/location/1",
+            created: "2017-11-10T12:42:04.162Z",
+          },
+        ],
+      }}
+    />
+  );
+
+  fireEvent.click(screen.getByRole("show-more-data"));
+
+  expect(screen.getByRole("show-more-data").className).toBe(
+    "location-card-viewport clicked"
+  );
 });
